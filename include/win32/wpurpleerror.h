90e885dac144d4fcc5bd817eb2a5b753497e1bf6
Andrew Nagle <kabili@zyrenth.com>
2016-02-19 14:23:17 -0500

    Initial commit

---
+++ include/win32/wpurpleerror.h
@@ -0,0 +1,31 @@
+/*
+ * purple
+ *
+ * File: wpurpleerror.h
+ * Date: October 14, 2002
+ * Description: Convert Winsock errors to Unix errors
+ *
+ * Copyright (C) 2002-2003, Herman Bloggs <hermanator12002@yahoo.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111-1301  USA
+ *
+ */
+#ifndef _WPURPLEERROR_H
+#define _WPURPLEERROR_H
+
+/* Here we define unix socket errors as windows socket errors */
+
+
+#endif /* end _WPURPLEERROR_H */
